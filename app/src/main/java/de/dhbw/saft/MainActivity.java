fee150b80aad660b97309a9e767a1af71dc4e11f
Maurice <maurice@cutecraft.net>
2025-11-07 12:43:44 +0100

    add: use builder to generate tiles & add icons

--- app/src/main/java/de/dhbw/saft/MainActivity.java
+++ app/src/main/java/de/dhbw/saft/MainActivity.java
@@ -1,35 +1,28 @@
 package de.dhbw.saft;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
-import androidx.gridlayout.widget.GridLayout;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+
+import de.dhbw.saft.common.TileBuilder;
 import de.dhbw.saft.databinding.ActivityMainBinding;
+import lombok.Getter;
 
 public class MainActivity extends AppCompatActivity {
-	private GridLayout tileGridLayout;
+
+	@Getter
+	private ActivityMainBinding binding;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 		Toolbar toolbar = binding.toolbar.findViewById(R.id.toolbar);
-		tileGridLayout = binding.tilesGridLayout;
 
 		setSupportActionBar(toolbar);
 
@@ -37,71 +30,21 @@ public class MainActivity extends AppCompatActivity {
 		toolbar.setNavigationIcon(R.drawable.baseline_home_24);
 		toolbar.setNavigationOnClickListener(v -> {});
 
-		buildDynamicallyTiles();
-	}
+		final TileBuilder builder = new TileBuilder(this);
+		builder
+				.addTile(
+						0,
+						R.drawable.tile_where2go,
+						"https://www.google.com/maps/d/embed?mid=1xRb0uZgr4Lsyys_mqYxPgT--4JO4OpA&ehbc=2E312F&ll=49.48905226213409%2C8.480855325064853&z=14"
+				).addTile(
+					2,
+						R.drawable.tile_moodle,
+					"https://moodle.dhbw-mannheim.de/login/index.php"
+				).addTile(
+						3,
+						R.drawable.tile_zimbra,
+						"https://studgate.dhbw-mannheim.de/"
 
-	private void openUrlInBrowser(@NotNull String url) {
-		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-		Intent chooser = Intent.createChooser(intent, getString(R.string.title_browser_chooser));
-
-		if (chooser.resolveActivity(getPackageManager()) != null) {
-			startActivity(chooser);
-			return;
-		}
-
-		Toast.makeText(this, R.string.error_text_no_app_found_chooser, Toast.LENGTH_SHORT).show();
-	}
-
-	private void buildDynamicallyTiles(){
-		final String[] ALL_TILES_TITLE = this.getResources().getStringArray(R.array.all_tiles_title);
-		final String[] URLS = {"https://dhbw.app/c/MA-TINF25AI1","","","","",""};
-		final int f1Img = R.drawable.baseline_account_balance_24;
-		final int[] ALL_ICONS_RESOURCES_IDS = {f1Img,f1Img,f1Img,f1Img,f1Img,f1Img};
-		final int TOTAL_CARDS = ALL_TILES_TITLE.length;
-		final int TILES_CARD_GRID_COLUMNS = 2;
-
-		for (int i = 0; i < TOTAL_CARDS; i++) {
-			CardView cardView = new CardView(this);
-
-			//TODO: DELETE IN PRODUCTION
-			if (i > 0) cardView.setVisibility(CardView.INVISIBLE);
-
-			GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-
-			params.width = 0;
-			params.height = 0;
-			params.columnSpec = GridLayout.spec(i % TILES_CARD_GRID_COLUMNS, 1f);
-			params.rowSpec = GridLayout.spec(i / TILES_CARD_GRID_COLUMNS, 1f);
-			params.setMargins(8, 8, 8, 8);
-
-			cardView.setLayoutParams(params);
-			cardView.setRadius(12);
-			cardView.setCardElevation(8);
-			cardView.setUseCompatPadding(true);
-
-			LinearLayout linearLayout = new LinearLayout(this);
-			linearLayout.setOrientation(LinearLayout.VERTICAL);
-			linearLayout.setGravity(Gravity.CENTER);
-			linearLayout.setPadding(8, 8, 8, 8);
-
-			TextView textView = new TextView(this);
-			textView.setText(ALL_TILES_TITLE[i]);
-			textView.setTextSize(24);
-			textView.setTextColor(ContextCompat.getColor(this, android.R.color.black));
-			textView.setGravity(Gravity.CENTER);
-
-			ImageView imageView = new ImageView(this);
-			imageView.setLayoutParams(new LinearLayout.LayoutParams(64, 64));
-			imageView.setImageResource(ALL_ICONS_RESOURCES_IDS[i]);
-
-			linearLayout.addView(textView);
-			linearLayout.addView(imageView);
-			cardView.addView(linearLayout);
-
-			int finalI = i;
-			cardView.setOnClickListener(view -> openUrlInBrowser(URLS[finalI]));
-
-			tileGridLayout.addView(cardView);
-		}
+				);
 	}
 }
