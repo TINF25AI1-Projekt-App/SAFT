a294ebff9338399014acb923bd402c6684a749e0
Maurice <maurice@cutecraft.net>
2025-12-10 10:13:03 +0100

    feat: implement course completion

--- app/src/main/java/de/dhbw/saft/fragment/HomeFragment.java
+++ app/src/main/java/de/dhbw/saft/fragment/HomeFragment.java
@@ -54,7 +54,7 @@ public class HomeFragment extends NamedFragment {
 		final TileBuilder builder = new TileBuilder(this);
 		builder.addLinkTile(0, R.drawable.tile_where2go,
 				"https://www.google.com/maps/d/embed?mid=1xRb0uZgr4Lsyys_mqYxPgT--4JO4OpA&ehbc=2E312F&ll=49.48905226213409%2C8.480855325064853&z=14")
-				.addWebViewTile(1, R.drawable.deutsche_bahn,
+				.addWebViewTile(1, R.drawable.deutsche_bahn, "Navigator",
 						"https://www.fahrplan.guru/haltestelle/deutschland/baden-wuerttemberg/mannheim/neuostheim-duale-hochschule")
 				.addLinkTile(2, R.drawable.tile_moodle, "https://moodle.dhbw-mannheim.de/login/index.php")
 				.addLinkTile(3, R.drawable.tile_zimbra, "https://studgate.dhbw-mannheim.de/");
